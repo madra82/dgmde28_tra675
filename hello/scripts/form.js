3d8fe9c6c4dc59774ecb3e9e85d4eb5f246d54d8
tarmstrong014 <90572273+tarmstrong014@users.noreply.github.com>
2022-02-08 20:36:57 -0500

    Add files via upload

---
+++ hello/scripts/form.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", function(){
+
+    let button = document.getElementById("subscribe")
+
+    button.addEventListener('submit',event=>{event.preventDefault(),alert("Thank you for your interest!")});
+
+});
